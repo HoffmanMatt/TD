8a7bfd4a5141e57653bc6f931fecd988aad63ae2
Matt Hoffman <mdh119@txstate.edu>
2016-05-08 15:55:59 -0500

    hi
    hi

---
+++ Assets/Scripts/Level_1_Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level_1_Enemy : MonoBehaviour {
+
+    //float maxHitPoints = 50;
+    public float hitPoints = 50;
+    
+
+    void Start()
+    {      
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Damage")
+        {
+            //var scriptName = "Tower_Rocket Projectile";
+            //need to work for all types of projectiles. not sure how
+            Tower_Rocket_Projectile_Yaxis theScript = (Tower_Rocket_Projectile_Yaxis) other.gameObject.GetComponent("Tower_Rocket_Projectile_Yaxis");
+            //hitPoints = hitPoints - 16;
+            hitPoints = hitPoints - theScript.damagePerAttack;
+            Destroy(other.gameObject);
+            if (hitPoints <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player_Info.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Info : MonoBehaviour {
+
+    public int playerID;
+    public string selectedRace;
+    public int livesRemaining = 30;
+    public PlayerManager playerManagerScript;
+    public GameObject playerManagerObject;
+    public GameObject playerUI;
+
+	// Use this for initialization
+	void Start () {
+        playerID = GetInstanceID();
+        playerManagerObject = GameObject.Find("PlayerManager");
+        playerManagerScript = (PlayerManager) playerManagerObject.GetComponent("PlayerManager");
+
+        // when this object is created, send this object to the manager so it can index it
+        playerManagerScript.addPlayer(transform.gameObject);
+
+        // attach this player_info object to the PlayerManager object.
+        playerManagerScript.setChildRelationship(transform.gameObject);
+        
+        // race select
+        selectTrads();
+
+        // instantiate and set up the interface, and then attach it to this object
+        generateUI();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void generateUI ()
+    {
+        playerUI = Instantiate(playerManagerScript.genericUI);
+        playerUI.transform.parent = transform.parent;
+    }
+
+
+    void selectTrads()
+    {
+        // set race. needs to be changed.
+        selectedRace = "Trads";
+        
+        // Remove the race selection UI
+        GameObject raceSelectionObject = GameObject.Find("RaceSelection UI");
+        Destroy(raceSelectionObject);
+        
+    }
+}
